18fdbf8be4394554904782574ce12f2082b00bb5
Leandro Portugal <leandroportugal@outlook.com.br>
2022-05-23 02:15:59 -0300

    project created

---
+++ src/main/java/br/com/takemehome/TakemehomeApplication.java
@@ -0,0 +1,13 @@
+package br.com.takemehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakemehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakemehomeApplication.class, args);
+	}
+
+}
