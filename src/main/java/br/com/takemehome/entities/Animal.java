fd0d5852aba54e13ec3f6ac868650d20fabdde63
paulotunis <paulotunis@hotmail.com>
2022-05-28 20:13:31 -0300

    add index.html

--- src/main/java/br/com/takemehome/entities/Animal.java
+++ src/main/java/br/com/takemehome/entities/Animal.java
@@ -2,13 +2,11 @@ package br.com.takemehome.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
 public class Animal implements Serializable {
